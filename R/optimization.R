87d4bfcc2d98585772382c9a2d76e0f63eb82462
Aaron Mackey <56453608+amackeyitx@users.noreply.github.com>
2020-04-16 13:50:24 -0400

    Update optimization.R
    remove getStart(lower=...) param entirely

--- R/optimization.R
+++ R/optimization.R
@@ -22,7 +22,7 @@ optimizeLmerCens <- function(devfun,
 
   opt <- optwrapCens(optimizer,
                      devfun,
-                     lme4:::getStart(start, lower=rho$lower, pred=rho$pp, returnVal = "all"),
+                     lme4:::getStart(start, pred=rho$pp, returnVal = "all"),
                      lower=rho$lower,
                      control=control,
                      verbose=verbose,
